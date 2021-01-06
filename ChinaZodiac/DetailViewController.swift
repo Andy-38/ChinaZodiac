c325d220988740d82afd23841020f23feb65950b
Andrey Dvoytsov <annihilator1981@mail.ru>
2021-01-06 22:10:28 +0500

    Remove the yearLabel in DetailViewController

--- ChinaZodiac/DetailViewController.swift
+++ ChinaZodiac/DetailViewController.swift
@@ -22,16 +22,33 @@ extension UIImage { // дополнительная функция для изм
     }
 }
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let maxYears = 10 // максимальное количество посчитанных лет для выбранного животного в одну сторону (прошлое/будущее)
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1 // количество крутящихся "барабанов"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return maxYears * 2 + 1 // количество элементов в каждом "барабане"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? { // содержимое строк в "барабане"
+        return String(years[row]) // заполняем строки годами
+    }
+    
+    
 
     var animal : String = "" // название животного
     var number: Int = 0 // порядковый номер в списке
     let firstYear: Int = 2016 // первый ближайщий год обезьяны
     var year = 2016 // центральный год на экране
+    var years = [Int] () // массив годов для выбранного животного
     
     @IBOutlet weak var yearLabel: UILabel!
-    
     @IBOutlet weak var yearImageView: UIImageView!
+    @IBOutlet weak var yearPickerView: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,12 +64,25 @@ class DetailViewController: UIViewController {
         yearImageView.layer.borderWidth = 1 // толщина рамки
         yearImageView.layer.borderColor = UIColor.black.cgColor // цвет рамки
         //year = firstYear + number - 1
-        yearLabel.text = String(year) // основной год
+        //yearLabel.text = String(year) // основной год
         //let width = 100
         //let height = 20
         //yearLabel.frame = CGRect(x: Int(view.frame.midX) - width/2, y: Int(view.frame.maxY) - height - 100, width: width, height: height)
         //yearLabel.frame.
-  
+        
+        // заполняем массив годов
+        for i in 0...maxYears - 1 {
+            years.append(year + 12 * (i - maxYears))
+        } // года до текущего
+        years.append(year) // текущий год
+        for i in 0...maxYears - 1 {
+            years.append(year + 12 * (i + 1))
+        } // года после текущего
+        
+        yearPickerView.dataSource = self
+        yearPickerView.delegate = self
+        
+        yearPickerView.selectRow(maxYears, inComponent: 0, animated: true) // выбираем текущий год в "барабане"
     }
     
 
