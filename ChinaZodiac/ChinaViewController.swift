334077e7ddb9d338f475b0a36d7f7672ae794e46
Andrey Dvoytsov <annihilator1981@mail.ru>
2021-01-07 00:18:52 +0500

    Added search by year

--- ChinaZodiac/ChinaViewController.swift
+++ ChinaZodiac/ChinaViewController.swift
@@ -11,12 +11,23 @@ import UIKit
 
 class ChinaViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    @IBOutlet weak var yearEdit: UITextField!
+    @IBOutlet weak var findButton: UIButton!
+    
     var animalsZodiac = ["Обезьяна", "Петух", "Собака", "Свинья (Кабан)", "Крыса (Мышь)", "Бык", "Тигр", "Кролик (Кот)", "Дракон", "Змея", "Лошадь", "Коза (Овца)"]
+    
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        // вызывается когда пользователь коснулся экрана
+//        super.touchesBegan(touches, with: event)
+//        view.endEditing(true) // убираем клавиатуру
+//        print(#line, #function)
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Символы года"
         navigationController?.navigationBar.prefersLargeTitles = true // большие заголовки
+        //yearEdit.layer.cornerRadius = 10
     }
     
     
@@ -25,7 +36,7 @@ class ChinaViewController: UIViewController, UITableViewDataSource, UITableViewD
         // сколько раз вызывается функция следующая
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { // заполняем список животными
         let cell = tableView.dequeueReusableCell(withIdentifier: "AnimalCell", for: indexPath)
         let animal = animalsZodiac[indexPath.row] // получаем элемент массива
         cell.textLabel?.text = animal // заголовок экрана - название животного
@@ -58,21 +69,31 @@ class ChinaViewController: UIViewController, UITableViewDataSource, UITableViewD
         let animal = animalsZodiac[indexPath.row] // получаем элемент массива
         
         detailViewController.animal = animal // передаем его на новый экран
-        detailViewController.number = indexPath.row+1
+        detailViewController.number = indexPath.row + 1
         detailViewController.year = detailViewController.firstYear + indexPath.row
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "AnimalCell", for: indexPath)
-//        detailViewController.image = cell.imageView?.image
-//
+
+        view.endEditing(true) // убираем клавиатуру
+        yearEdit.text = "" // обнуляем год в поле ввода
         navigationController?.pushViewController(detailViewController, animated: true)
     }
-    /*
-    // MARK: - Navigation
+    
+    
+    @IBAction func onFindButtonClick(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        let detailViewController = storyboard.instantiateViewController(identifier: "YearDetail") as! DetailViewController // при нажатии открываем подробный экран
+        
+        guard let year = Int(yearEdit.text ?? "") else {return}
+        let numAnimal = year % 12 // узнаем номер животного для введенного года
+        let animal = animalsZodiac[numAnimal] // получаем элемент массива
+        
+        detailViewController.animal = animal // передаем его на новый экран
+        detailViewController.number = numAnimal + 1
+        detailViewController.year = year
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        view.endEditing(true) // убираем клавиатуру
+        yearEdit.text = "" // обнуляем год в поле ввода
+        navigationController?.pushViewController(detailViewController, animated: true)
     }
-    */
+    
 
 }
