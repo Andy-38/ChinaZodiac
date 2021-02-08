8e068c58140befb3373efb1281121c5de49a1a99
Andrey Dvoytsov <annihilator1981@mail.ru>
2021-02-08 12:13:57 +0500

    some changes for MVC architecture

--- ChinaZodiac/DetailViewController.swift
+++ ChinaZodiac/DetailViewController.swift
@@ -8,65 +8,47 @@
 import UIKit
 
 class DetailViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
-    
-    let maxYears = 10 // максимальное количество посчитанных лет для выбранного животного в одну сторону (прошлое/будущее)
+    var zodiacModel : ZodiacModel = ZodiacModel() // модель для работы с животными по китайскому календарю
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1 // количество крутящихся "барабанов"
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return maxYears * 2 + 1 // количество элементов в каждом "барабане"
+        return zodiacModel.numberOfElements // количество элементов в каждом "барабане"
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? { // содержимое строк в "барабане"
-        return String(years[row]) // заполняем строки годами
+        return String(zodiacModel.years[row]) // заполняем строки годами
     }
-    
-    
 
-    var animal : String = "" // название животного
-    var number: Int = 0 // порядковый номер в списке
-    var year = 2016 // центральный год на экране
-    var years = [Int] () // массив годов для выбранного животного
+    var animal: String = "" // название животного
+    var number: Int = 0     // порядковый номер в списке
+    var year:   Int = 0     // центральный год на экране
     
     @IBOutlet weak var yearImageView: UIImageView!
     @IBOutlet weak var yearPickerView: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.title = animal
+        navigationItem.title = animal //заголовок экрана - выбранное животное
         navigationController?.navigationBar.prefersLargeTitles = true // большие заголовки
-        let imageName = "\(number+1).png"
+        
+        let imageName = zodiacModel.getImageName(index: number) // путь к картинке
         let image = UIImage(named: imageName) // грузим картинку из файла
         let sizeOfImage = view.frame.midX // размер картинки - половина экрана
         yearImageView.image = image?.resize(sizeOfImage,sizeOfImage) // изменяем размер
-        
         yearImageView.clipsToBounds = true // обрезаем картинку согласно слою Layer
         yearImageView.layer.cornerRadius = sizeOfImage / 2 // скругляем углы
         yearImageView.layer.borderWidth = 1 // толщина рамки
         yearImageView.layer.borderColor = UIColor.black.cgColor // цвет рамки
-        //year = firstYear + number - 1
-        //yearLabel.text = String(year) // основной год
-        //let width = 100
-        //let height = 20
-        //yearLabel.frame = CGRect(x: Int(view.frame.midX) - width/2, y: Int(view.frame.maxY) - height - 100, width: width, height: height)
-        //yearLabel.frame.
         
         // заполняем массив годов
-        for i in 0...maxYears - 1 {
-            years.append(year + 12 * (i - maxYears))
-        } // года до текущего
-        years.append(year) // текущий год
-        for i in 0...maxYears - 1 {
-            years.append(year + 12 * (i + 1))
-        } // года после текущего
+        zodiacModel.getYearsArray(year: year)
         
-        yearPickerView.dataSource = self
-        yearPickerView.delegate = self
+        yearPickerView.dataSource = self // источник данных
+        yearPickerView.delegate = self // делегат
         
-        yearPickerView.selectRow(maxYears, inComponent: 0, animated: true) // выбираем текущий год в "барабане"
+        yearPickerView.selectRow(zodiacModel.maxYears, inComponent: 0, animated: true) // выбираем текущий год в "барабане"
     }
-
-
 }
